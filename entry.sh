e445c1ab89fc8631ce840016fa48c6a221a36f32
Gonzalo Peci <pecigonzalo@outlook.com>
2017-04-19 17:12:43 +0200

    Prevent unbound vars from terminating

--- entry.sh
+++ entry.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-set -e          # exit on command errors
-set -o nounset  # abort on unbound variable
+set -e # exit on command errors
+# set -o nounset  # abort on unbound variable
 set -o pipefail # capture fail exit codes in piped commands
 
 echo "#================"
